32237f368d6ecdc96f64c854bfcd16bb4b950d43
Choihyeongu <a11dsd123@gmail.com>
2023-10-04 10:02:40 +0900

    init: 프로젝트 생성

---
+++ react-native.js
@@ -0,0 +1,44 @@
+module.exports = {
+  extends: ['plugin:react/recommended', './index.js'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+  },
+  plugins: ['react', '@react-native-community'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'index'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'react-native',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
