8b8ee34df67c63bba99a9f6a85a6e27912fc9b62
Choihyeongu <a11dsd123@gmail.com>
2024-08-08 20:17:56 +0900

    refact: Rule 수정 및 주석 추가

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 module.exports = {
+  // 이 설정이 프로젝트의 루트에 위치한다는 것을 명시합니다.
   root: true,
   extends: ['eslint:recommended'],
   env: {
     node: true,
   },
+
+  // 규칙들을 설정합니다.
   rules: {
+    // 모든 제어 구조에 중괄호 사용을 강제합니다.
     curly: ['error', 'all'],
-    // 들여쓰기 깊이 제한
+
+    // 들여쓰기 깊이를 최대 2단계로 제한합니다.
     'max-depth': ['error', 2],
-    // 함수의 파라미터 개수 제한
+
+    // 함수의 파라미터 개수를 최대 3개로 제한합니다.
     'max-params': ['error', 3],
-    // 모든 리터럴이 큰따옴표로 사용되게 제한
-    quotes: [2, 'double', {avoidEscape: true}],
-    // 후행 쉼표 제한
+
+    // 모든 문자열 리터럴에 큰따옴표를 사용하도록 강제하며, 이스케이프가 필요한 경우 예외를 허용합니다.
+    quotes: ['error', 'double', {avoidEscape: true}],
+
+    // 후행 쉼표에 대한 규칙을 설정합니다.
     'comma-dangle': [
       'warn',
       {
--- react-native.js
+++ react-native.js
@@ -1,20 +1,49 @@
 module.exports = {
+  // 기본적으로 React 관련 권장 설정을 확장합니다.
   extends: ['plugin:react/recommended', './index.js'],
+
+  // TypeScript를 사용하기 위해서 파서를 설정합니다.
   parser: '@typescript-eslint/parser',
+
   parserOptions: {
+    // 모듈 시스템을 사용할 수 있게 합니다.
     sourceType: 'module',
+
+    // JSX를 사용할 수 있도록 설정합니다.
     ecmaFeatures: {
       jsx: true,
     },
+
+    // ES11 (ES2020)을 사용합니다.
     ecmaVersion: 11,
   },
+
   plugins: ['react', '@react-native-community', 'import'],
+
   rules: {
+    // React 17부터는 JSX에서 React를 자동으로 가져오므로, React를 스코프에 포함하지 않아도 됩니다.
     'react/react-in-jsx-scope': 'off',
+
+    // 여러 줄로 된 항목에서만 후행 쉼표를 허용합니다.
+    'comma-dangle': ['error', 'only-multiline'],
+
+    // 사용하지 않는 import를 금지합니다.
+    'unused-imports/no-unused-imports': 'error',
+
+    // React hooks의 규칙을 강제합니다.
+    'react-hooks/rules-of-hooks': 'error',
+
+    // React hooks의 의존성 배열에 대한 경고를 표시합니다.
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // import 정렬 순서를 정의합니다.
     'import/order': [
       'error',
       {
+        // import 그룹의 순서를 설정합니다.
         groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'unknown', 'type'],
+
+        // 특정 경로 패턴의 import를 그룹 내에서 재배치합니다.
         pathGroups: [
           {
             pattern: 'react',
@@ -24,6 +53,11 @@ module.exports = {
           {
             pattern: 'react-native',
             group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@**',
+            group: 'internal',
             position: 'before',
           },
           {
@@ -37,11 +71,14 @@ module.exports = {
             position: 'before',
           },
         ],
-        pathGroupsExcludedImportTypes: ['react'],
+
+        // import를 알파벳 순으로 정렬합니다.
         alphabetize: {
-          order: 'asc',
-          caseInsensitive: false,
+          order: 'asc', // 오름차순으로 정렬
+          caseInsensitive: false, // 대소문자 구분을 설정
         },
+
+        // import 간에 새로운 줄을 추가합니다.
         'newlines-between': 'always',
       },
     ],
